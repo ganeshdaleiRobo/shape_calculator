13d21865920d9b469b8fac0b8e5def88b23b4f58
ganesh <ganeshdalei1992@gmail.com>
2020-06-16 09:01:22 +0530

    first-commit

---
+++ src/styles/style.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/style'
+
+export const Header = styled.viv`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+`
